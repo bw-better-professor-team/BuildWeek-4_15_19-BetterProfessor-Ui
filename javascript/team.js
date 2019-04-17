a0dfc69d46ca29ba1bcb6bf69ae612dfac741533
tjr226 <tjr226@gmail.com>
2019-04-17 17:38:42 -0400

    improvements to team components
    added headshots and bio templates for each team member. got formatting of selected components to behave as desired, including breakpoints for the components. still a few formatting tweaks to be done to the tablet and smaller versions of the components page.

--- javascript/team.js
+++ javascript/team.js
@@ -2,61 +2,41 @@ class TeamMemberButton {
   constructor(button){
     // assign this.tabElement to the tabElement DOM reference
     this.button = button;
-    console.log("this.button");
-    console.log(this.button);
+    // console.log("this.button");
+    // console.log(this.button);
 
-    // Get the `data-tab` value from this.tabElement and store it here
-    // this.tabData = this.tabElement.dataset.tab; 
-    console.log(this.button.dataset);
+    // console.log(this.button.dataset);
     this.buttonData = this.button.dataset.lastname;
-    console.log("this.buttonData");
-    console.log(this.buttonData)
-
-    // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
-    // if(this.tabData === "all"){
-      // If `all` is true, select all cards regardless of their data attribute values
-      // this.cards = document.querySelectorAll('.card');
-    // } else {
-      // else if `all` is false, only select the cards with matching this.tabData values
-    //   this.cards = document.querySelectorAll(`.card[data-tab="${this.tabData}"]`);
-    // }
+    // console.log("this.buttonData");
+    // console.log(this.buttonData)
 
     this.lastnameBio = document.querySelector(`.team-member-bio[data-lastname="${this.buttonData}"]`)
-    console.log("this.lastnameBio");
-    console.log(this.lastnameBio);
-
-    
-     // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-    // this.cards = Array.from(this.cards).map(card => new TabCard(card));
-
-    // this.bioSection = new BioSection(this.lastNameBio);
-
-    // Add a click event that invokes this.selectTab
-    // this.tabElement.addEventListener('click', this.selectTab.bind(this));
+    // console.log("this.lastnameBio");
+    // console.log(this.lastnameBio);
 
     this.button.addEventListener('click', this.selectButton.bind(this));
   }
 
   selectButton() {
-    // Select all elements with the .tab class on them
-    // const tabs = document.querySelectorAll('.tab');
+
 
     const buttons = document.querySelectorAll('.team-member-button');
-
-
-    // Iterate through the NodeList removing the .active-tab class from each element
-    // tabs.forEach( tab => { tab.classList.remove('active-tab')})
 
     buttons.forEach( button => { button.classList.remove('active-button')});
     buttons.forEach( button => { button.classList.add('inactive-button')});
 
-    const bios = document.querySelectorAll('.team-member-bio');
-    bios.forEach( bio => { bio.style.display = 'none'});
-
-    this.lastnameBio.style.display = 'flex';
     this.button.classList.remove('inactive-button');
     this.button.classList.add('active-button');
 
+
+    const bios = document.querySelectorAll('.team-member-bio');
+    bios.forEach( bio => { bio.classList.remove('active-bio')});
+    bios.forEach( bio => { bio.classList.add('inactive-bio')});
+
+    this.lastnameBio.classList.remove('inactive-bio');
+    this.lastnameBio.classList.add('active-bio');
+
+    
   }
 }
 
