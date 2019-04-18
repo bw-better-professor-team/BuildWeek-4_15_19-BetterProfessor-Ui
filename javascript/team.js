286922f0b7f230bae7f493089abac5be31fe7421
tjr226 <tjr226@gmail.com>
2019-04-18 16:03:48 -0400

    added comments for increased clarity

--- javascript/team.js
+++ javascript/team.js
@@ -1,7 +1,9 @@
 class TeamMemberButton {
+  // button constructor
   constructor(button) {
     this.button = button;
     this.buttonData = this.button.dataset.lastname;
+    // data selector in HTML is lower case last names
     this.lastnameBio = document.querySelector(`.team-member-bio[data-lastname="${this.buttonData}"]`)
     this.button.addEventListener('click', this.selectButton.bind(this));
   }
@@ -10,20 +12,31 @@ class TeamMemberButton {
 
     const buttons = document.querySelectorAll('.team-member-button');
 
+    // remove any active-button classes and toggle the selected button
     buttons.forEach(button => { button.classList.remove('active-button') });
     this.button.classList.toggle('active-button');
 
     const bios = document.querySelectorAll('.team-member-bio');
-    bios.forEach(bio => { bio.classList.remove('active-bio') });
 
+    // remove any active bios and toggle the selected bio
+    bios.forEach(bio => { bio.classList.remove('active-bio') });
     this.lastnameBio.classList.toggle('active-bio');
 
   }
 }
 
+
+
+// grab teamMemberButtons from document
+
 let teamMemberButtons = document.querySelectorAll('.team-member-button');
 
+// convert to array
+
 teamMemberButtons = Array.from(teamMemberButtons);
+
+// use map() array method from stretch goals to return new components
+
 teamMemberButtons = teamMemberButtons.map(button => new TeamMemberButton(button));
 
 
