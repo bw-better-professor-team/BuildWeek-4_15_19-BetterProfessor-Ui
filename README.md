019f3aa1f091286aeabbc7a480d3c6ff8bf3b768
tjr226 <tjr226@gmail.com>
2019-04-18 13:06:48 -0400

    reformat code, wrote readme
    simple commit in terms of code. wrote the Readme. formatted all LESS and JS files for readability, removed unnecessary comments from JS.

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 UI Project
 
 Page style will take inspiration from https://html5up.net/stellar
+
+Guide for future devs:
+
+-Index.html was the main page. For team.html, I copied the HTML from index.html, deleted all the feature divs, and put the team bios in that place instead. 
+
+-You will need to use a LESS compiler, all CSS code was written in LESS files and then compiled.
+
+-Team bio buttons are components created in team.js. 
+
+-All icons and photos in "assets" folder.
--- javascript/team.js
+++ javascript/team.js
@@ -1,60 +1,34 @@
 class TeamMemberButton {
-  constructor(button){
-    // assign this.tabElement to the tabElement DOM reference
+  constructor(button) {
     this.button = button;
-    // debugger;
-    // console.log("this.button");
-    // console.log(this.button);
-
-    // console.log(this.button.dataset);
     this.buttonData = this.button.dataset.lastname;
-    // console.log("this.buttonData");
-    // console.log(this.buttonData)
-
     this.lastnameBio = document.querySelector(`.team-member-bio[data-lastname="${this.buttonData}"]`)
-    // console.log("this.lastnameBio");
-    // console.log(this.lastnameBio);
-
     this.button.addEventListener('click', this.selectButton.bind(this));
   }
 
   selectButton() {
 
-
     const buttons = document.querySelectorAll('.team-member-button');
 
-    buttons.forEach( button => { button.classList.remove('active-button')});
-    buttons.forEach( button => { button.classList.add('inactive-button')});
+    buttons.forEach(button => { button.classList.remove('active-button') });
+    buttons.forEach(button => { button.classList.add('inactive-button') });
 
     this.button.classList.remove('inactive-button');
     this.button.classList.add('active-button');
 
-
     const bios = document.querySelectorAll('.team-member-bio');
-    bios.forEach( bio => { bio.classList.remove('active-bio')});
-    bios.forEach( bio => { bio.classList.add('inactive-bio')});
+    bios.forEach(bio => { bio.classList.remove('active-bio') });
+    bios.forEach(bio => { bio.classList.add('inactive-bio') });
 
     this.lastnameBio.classList.remove('inactive-bio');
     this.lastnameBio.classList.add('active-bio');
 
-    
   }
 }
 
-
 let teamMemberButtons = document.querySelectorAll('.team-member-button');
-
-// console.log("start at bottom of page");
-// console.log(teamMemberButtons);
-
-// console.log(teamMemberButtons);
 
 teamMemberButtons = Array.from(teamMemberButtons);
 teamMemberButtons = teamMemberButtons.map(button => new TeamMemberButton(button));
-
-// teamMemberButtons.forEach(function(button){
-//   return new TeamMemberButton(button);
-// });
-
 
 
